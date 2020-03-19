efdf8db762a26a3f00e2b2e3bed4419aaa7cf198
Wilkor <wilkorguitarrista@msn.com>
2020-03-19 20:54:37 +0000

    mudando versão do socket

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const app = express();
 const http = require('http').Server(app);
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
 
-    const io = require('socket.io')(http);
-    const cors = require('cors');
-    const connectedUser = {}
+const io = require('socket.io')(http);
+const cors = require('cors');
+const connectedUser = {}
     
- io.listen(http, {log:false, origins:'*:*'});
 io.on('connection', socket => {
   socket.on("notification", data =>{
     if(data.profileType === "admin"){
