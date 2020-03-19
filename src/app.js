a3f530ec949775c26f0565508c327d060e1734c4
Wilkor <wilkorguitarrista@msn.com>
2020-03-19 14:56:36 +0000

    ajuste de cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Origin": "*", //or the specific origin you want to give access to,
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
